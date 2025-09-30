015c02f5df0f2a36b1e90a7055fe479309c30b0e
DavidEIG12 <amm24gr025@student.esgerencia.com>
2025-09-30 12:06:21 +0200

    Nuevo componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Contacto } from './contacto/contacto';
+import { Multimedia } from './multimedia/multimedia';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Contacto],
+  imports: [RouterOutlet, Contacto, Multimedia],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
